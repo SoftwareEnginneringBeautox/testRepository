772e4f7d79beeb68149c8335c1915bbaf1f3ce94
Ivan <47345247+ivanmendozalawrence@users.noreply.github.com>
2025-01-18 03:59:59 +0800

    Update
    Working Postgres server and data base back end only missing front end react and config files that will be ignored

---
+++ Server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const pool = require("./database.js");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+//localhost:3000
+//localhost:4000
+app.post('/adduser', (req, res) => {
+    const id = req.body["id"];
+    const username = req.body["username"];
+    const password = req.body["password"];
+
+    console.log("id"+id);
+    console.log("username"+username);
+    console.log("password"+password);
+
+    const insertst =`INSERT INTO accounts (id, username, password) VALUES ( '${id}','${username}','${password}');`;
+    pool.query(insertst).then((response) =>
+    {
+        console.log(response);
+        res.send("User Added");
+    }).catch((error) =>
+    {
+        console.log(error);
+    });
+
+    console.log(req.body);
+    res.send("response Revieced"+req.body);
+});
+app.listen(4000, () => {
+  console.log('Server is running on port 4000');
+});
--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ const App = () => {
     <div>
       <h1>React App</h1>
     </div>
+    
   );
 }
 
