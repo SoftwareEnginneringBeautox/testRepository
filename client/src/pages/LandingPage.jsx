2fbbbe8273ad5b4a46fda3f2abfdb61e6f9750b5
DevT3MPEST <elilego120@gmail.com>
2025-03-07 19:46:02 +0800

    removed allow fullscreen

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -56,7 +56,6 @@ function LandingPage() {
           <div className="w-1/2 flex items-center justify-center rounded-half">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1043.644141889592!2d121.03651836623278!3d14.614630075990188!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b7656817cb95%3A0xc3a3721da76b89!2sSonema%20Square!5e0!3m2!1sen!2sph!4v1741322385811!5m2!1sen!2sph"
-              allowFullScreen="true"
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
               className="rounded-3xl w-full min-h-10"
