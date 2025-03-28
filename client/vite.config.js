b9c9ec9326d22ba200df1c0d95cd5abe4bc388bf
Ivan <47345247+ivanmendozalawrence@users.noreply.github.com>
2025-03-29 05:55:29 +0800

    A lot of changes please pull ill explain later

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,18 @@
-import path from "path";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tailwindcss from "tailwindcss";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
+  base: "./", // Ensures relative paths in production
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "src"),
     },
   },
   css: {
