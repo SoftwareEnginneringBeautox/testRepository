ceaae2fc293211993036fb0db233b570a642d602
DevT3MPEST <elilego120@gmail.com>
2025-03-31 02:10:03 +0800

    Replaced HoverCard with Drawer

--- client/src/components/ui/Carousel.jsx
+++ client/src/components/ui/Carousel.jsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import useEmblaCarousel from "embla-carousel-react";
 
 import { cn } from "@/lib/utils";
-// import { Button } from "@/components/ui/Button";
 import LargeChevronLeftIcon from "@/assets/icons/LargeChevronLeftIcon";
 import LargeChevronRightIcon from "@/assets/icons/LargeChevronRightIcon";
 
@@ -10,11 +9,8 @@ const CarouselContext = React.createContext(null);
 
 function useCarousel() {
   const context = React.useContext(CarouselContext);
-
-  if (!context) {
+  if (!context)
     throw new Error("useCarousel must be used within a <Carousel />");
-  }
-
   return context;
 }
 
@@ -32,85 +28,57 @@ const Carousel = React.forwardRef(
     ref
   ) => {
     const [carouselRef, api] = useEmblaCarousel(
-      {
-        ...opts,
-        axis: orientation === "horizontal" ? "x" : "y"
-      },
+      { ...opts, axis: orientation === "horizontal" ? "x" : "y" },
       plugins
     );
-    const [canScrollPrev, setCanScrollPrev] = React.useState(false);
-    const [canScrollNext, setCanScrollNext] = React.useState(false);
 
-    const onSelect = React.useCallback((api) => {
-      if (!api) {
-        return;
-      }
+    const [state, setState] = React.useState({
+      canScrollPrev: false,
+      canScrollNext: false,
+      isHovering: false
+    });
 
-      setCanScrollPrev(api.canScrollPrev());
-      setCanScrollNext(api.canScrollNext());
+    const updateScrollState = React.useCallback((api) => {
+      if (!api) return;
+      setState((prev) => ({
+        ...prev,
+        canScrollPrev: api.canScrollPrev(),
+        canScrollNext: api.canScrollNext()
+      }));
     }, []);
 
-    const scrollPrev = React.useCallback(() => {
-      api?.scrollPrev();
-    }, [api]);
-
-    const scrollNext = React.useCallback(() => {
-      api?.scrollNext();
-    }, [api]);
-
-    const handleKeyDown = React.useCallback(
-      (event) => {
-        if (event.key === "ArrowLeft") {
-          event.preventDefault();
-          scrollPrev();
-        } else if (event.key === "ArrowRight") {
-          event.preventDefault();
-          scrollNext();
-        }
-      },
-      [scrollPrev, scrollNext]
-    );
+    const scrollPrev = () => api?.scrollPrev();
+    const scrollNext = () => api?.scrollNext();
 
     React.useEffect(() => {
-      if (!api || !setApi) {
-        return;
-      }
-
+      if (!api || !setApi) return;
       setApi(api);
     }, [api, setApi]);
 
     React.useEffect(() => {
-      if (!api) {
-        return;
-      }
-
-      onSelect(api);
-      api.on("reInit", onSelect);
-      api.on("select", onSelect);
-
-      return () => {
-        api?.off("select", onSelect);
-      };
-    }, [api, onSelect]);
+      if (!api) return;
+      updateScrollState(api);
+      api.on("reInit", updateScrollState);
+      api.on("select", updateScrollState);
+      return () => api?.off("select", updateScrollState);
+    }, [api, updateScrollState]);
 
     return (
       <CarouselContext.Provider
         value={{
           carouselRef,
-          api: api,
+          api,
           opts,
-          orientation:
-            orientation || (opts?.axis === "y" ? "vertical" : "horizontal"),
+          orientation,
           scrollPrev,
           scrollNext,
-          canScrollPrev,
-          canScrollNext
+          ...state,
+          setState
         }}
       >
         <div
           ref={ref}
-          onKeyDownCapture={handleKeyDown}
-          className={cn("relative", className)}
+          className={cn("relative py-4", className)}
           role="region"
           aria-roledescription="carousel"
           {...props}
@@ -125,9 +93,8 @@ Carousel.displayName = "Carousel";
 
 const CarouselContent = React.forwardRef(({ className, ...props }, ref) => {
   const { carouselRef, orientation } = useCarousel();
-
   return (
-    <div ref={carouselRef} className="overflow-hidden">
+    <div ref={carouselRef} className="overflow-hidden min-h-32 pb-4">
       <div
         ref={ref}
         className={cn(
@@ -144,15 +111,14 @@ CarouselContent.displayName = "CarouselContent";
 
 const CarouselItem = React.forwardRef(({ className, ...props }, ref) => {
   const { orientation } = useCarousel();
-
   return (
     <div
       ref={ref}
       role="group"
       aria-roledescription="slide"
       className={cn(
-        "min-w-0 shrink-0 grow-0 basis-full",
-        orientation === "horizontal" ? "pl-4" : "pt-4",
+        "min-w-0 shrink-0 grow-0 basis-full bg-ash-200 hover:shadow-custom transition-all rounded-lg py-2",
+        orientation === "horizontal" ? "ml-4" : "mt-4",
         className
       )}
       {...props}
@@ -164,15 +130,13 @@ CarouselItem.displayName = "CarouselItem";
 const CarouselPrevious = React.forwardRef(
   ({ className, variant = "outline", size = "icon", ...props }, ref) => {
     const { orientation, scrollPrev, canScrollPrev } = useCarousel();
-
     return (
       <button
         ref={ref}
         variant={variant}
         size={size}
         className={cn(
-          "absolute h-16 w-8 transition-all hover:backdrop-blur-md hover:bg-white/10 border border-transparent ",
-          "flex items-center justify-center rounded-lg hover:shadow-md",
+          "absolute h-16 w-8 transition-all hover:backdrop-blur-md hover:bg-white/10 border border-transparent flex items-center justify-center rounded-lg hover:shadow-md",
           orientation === "horizontal"
             ? "-left-12 top-1/2 -translate-y-1/2"
             : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -194,15 +158,13 @@ CarouselPrevious.displayName = "CarouselPrevious";
 const CarouselNext = React.forwardRef(
   ({ className, variant = "outline", size = "icon", ...props }, ref) => {
     const { orientation, scrollNext, canScrollNext } = useCarousel();
-
     return (
       <button
         ref={ref}
         variant={variant}
         size={size}
         className={cn(
-          "absolute h-16 w-8 transition-all hover:backdrop-blur-md hover:bg-white/10 border border-transparent ",
-          "flex items-center justify-center rounded-lg hover:shadow-md",
+          "absolute h-16 w-8 transition-all hover:backdrop-blur-md hover:bg-white/10 border border-transparent flex items-center justify-center rounded-lg hover:shadow-md",
           orientation === "horizontal"
             ? "-right-12 top-1/2 -translate-y-1/2"
             : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
